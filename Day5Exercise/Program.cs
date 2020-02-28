63cd0ffbf88698e14aecb5367d2dc513c7c227d1
staceyloulouse <staceyloulouse@gmail.com>
2020-02-28 13:04:34 -0500

    Finishing touches put on Day 5 exercise

--- Day5Exercise/Program.cs
+++ Day5Exercise/Program.cs
@@ -7,21 +7,23 @@ namespace Day5Exercise
         static void Main(string[] args)
         {
             //get a phrase and create the acronym
-            string words = GetInput("Please enter words to create the Acronym: ");
+            string words = GetInput("Please enter some words to get the Acronym: ");
             ShowAcronym(words);
 
         }
         static string GetInput(string message)
         {
+            //write a prompt for the user to provide iput and return the 
             Console.Write(message);
             string wordSet = Console.ReadLine();
             return wordSet;
         }
         static void ShowAcronym(string phrase)
         {
+            //split the phrase, get the 1st initial of each word
             string[] parts = phrase.Split(" ");
             char[] initials = new char[parts.Length];
-            for (int i=0; i<parts.Length;i++)
+            for (int i = 0; i < parts.Length;i++)
             {
                 //prints the uppercase version of each initial in the set 
                 parts[i] = parts[i].ToUpper();
@@ -29,6 +31,7 @@ namespace Day5Exercise
                 initials[i] = firstCap;
                 Console.Write(firstCap);
             }
+            //finish the output with a summary
             Console.WriteLine($" is the Acronym for: {phrase}");
         }
     }
