0b2f5929faa6c81964b97262361733189fa67960
staceyloulouse <staceyloulouse@gmail.com>
2020-03-05 10:43:16 -0500

    completed buble sort in Day8Exercise

---
+++ CSharpExercises/BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 3, 4, 2, 1, 6 };
+            Console.WriteLine("before sort: [" + string.Join(",", array1) + "]");
+
+            BubbleSort(array1);
+            Console.WriteLine("after sort: [" + string.Join(",", array1) + "]");
+        }
+
+        static void BubbleSort(int[] array)
+        {
+            int swapCount = 1;
+            // 1. look at each consecutive pair,
+            do
+            {
+                swapCount = 0;
+                for (int i=0; i<array.Length-1;i++)
+                {
+                    if (array[i]>array[i+1])
+                    {
+                        //swap the values in the adjacent elements
+                        temp = array[i];
+                        arr[i] = arr[i+1];
+                        arr[i+1] = temp;
+                        swapCount++;
+                    }
+                    
+                } 
+            } while (swapCount>0);
+            //    swap them if they are not in order
+
+            // 2. do it again, until you go a whole
+            //    pass without making any swaps
+        }
+    }
+}
---
+++ CSharpExercises/Day8Exercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Day8Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to bubble sorter!");
+            int[] arr = {3,2,1,4,6};
+            int temp = 0;
+            bool wasChanged = true;
+            while(wasChanged)
+            {
+                wasChanged = false;
+                for (int i=0; i<arr.Length-1;i++)
+                {
+                    if (arr[i]>arr[i+1])
+                    {
+                        //swap the values in the adjacent elements
+                        temp = arr[i];
+                        arr[i] = arr[i+1];
+                        arr[i+1] = temp;
+                        wasChanged = true;
+                    }
+                    
+                }
+            }
+            PrintArray(arr);
+        }
+        static void PrintArray(int[] a)
+        {
+            for(int i = 0; i<a.Length; i++)
+            {
+                Console.Write(a[i]+" ");
+                
+            }
+            Console.WriteLine();
+        }
+    }
+}
